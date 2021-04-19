96ac00aca3f39fc9553cc77c84ef4f397d796c49
cornelia chirila <cornelia.calarasiu@yahoo.it>
2021-04-19 12:43:19 +0300

    madlib game

---
+++ madlib-game/madlib.py
@@ -0,0 +1,18 @@
+from stories import print_random
+#string concatenation
+#There are more ways to execute the string concatenation in python
+#youtuber = input("youtuber: ")
+
+#print("Subscribe to "+youtuber)
+#print("Subscribe to {}".format(youtuber))
+#print(f"Subscribe to {youtuber}")
+#verb1=input("Verb1: ")
+#adv1=input("Adv1: ")
+
+
+#madlib = f"I love to {verb1} because it makes me see the world from different points of view. \
+#Stay {adv1} as this will keep you young. "
+
+#print(madlib)
+
+print_random()
---
+++ madlib-game/stories.py
@@ -0,0 +1,17 @@
+def print_random():
+  adverb1 = input("adverb: ")
+  adverb2 = input("adverb: ")
+  adjective1 = input("adjective: ")
+  adjective2 = input("adjective: ")
+  adjective3 = input("adjective: ")
+  adjective4 = input("adjective: ")
+  noun1 = input("Noun: ")
+  noun2 = input("Noun: ")
+  noun3 = input("Noun: ")
+  verb1 = input("verb: ")
+  verb2 = input("verb: ")
+  verb3 = input("verb: ")
+
+  zoo = f"Today I went to the zoo. I saw a(n){adjective1} {noun1.capitalize()} jumping up and down in its tree.\
+  He {verb1} {adverb1} through the large tunnel that led to its {adjective2} {noun2}. I got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head. Feeding that animal made me hungry. I went to get a {adjective3} scoop of ice cream. It filled my stomach. Afterwards I had to {verb2}  {adverb2} to catch our bus. When I will get home I will {verb3} my mom for a {adjective4} day at the zoo."
+  print(zoo)
